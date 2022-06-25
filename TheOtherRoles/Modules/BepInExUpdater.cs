53f7f4fb771cc47285c555eae53cf6d1d9f6ca28
AlerHuGhe$ <axiasiming@gmail.com>
2022-06-25 22:26:34 +0800

    1

--- TheOtherRoles/Modules/BepInExUpdater.cs
+++ TheOtherRoles/Modules/BepInExUpdater.cs
@@ -15,9 +15,9 @@ using HarmonyLib;
 using UnityEngine;
 using UnityEngine.Networking;
 
-namespace TheOtherRoles.Modules;
-
-public class BepInExUpdater : MonoBehaviour
+namespace TheOtherRoles.Modules
+{
+    public class BepInExUpdater : MonoBehaviour
 {
     public const string MinimumBepInExVersion = "6.0.0.559";
     public const string BepInExDownloadURL = "https://builds.bepinex.dev/projects/bepinex_be/559/BepInEx_UnityIL2CPP_x86_fba4461_6.0.0-be.559.zip";
@@ -75,3 +75,5 @@ public static class StopLoadingMainMenu
         return !BepInExUpdater.UpdateRequired;
     }
 }
+}
+
